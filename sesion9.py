eb5b61e339bccb3ca335bbf3267bf16516e3934f
sandra <sandrahiguitatoro@gmail.com>
2023-10-14 17:35:39 -0500

    c1

---
+++ sesion9.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as pyplot
+
+data= pd.read_csv('ventas_vehiculos .csv')
+st.header('TABLA PRINCIPAL')
+data
+
+st.header("Antes de limpieza de datos")
+data
+#datos vacios
+data = data.dropna()
+
+#datos atipicos
+#plt.hist(data["Kilometraje"])
+#st.pyplot()
+
+#plt.boxplot(data["Kilometraje"])
+#st.pyplot()
+
+#data.plot.scatter(x="Estado", y="Kilometraje")
+#st.pyplot()
+
+data = data[~((data["Estado"] == "Nuevo") & (data["Kilometraje"] > 0))]
+
+#datos duplicados
+duplicates = data[data.duplicated(keep=False)]
+#st.write(duplicates)
+
+#formato de datos
+#st.write(data.dtypes)
+data["Fecha"] = pd.to_datetime(data["Fecha"])
+#st.write(data.dtypes)
+
+st.header("Después de Limpieza de Datos")
+data
